5d4d994d1894468a4cdddd6f82a091cf77746ada
AKSHAT YADAV <akshat0658@gmail.com>
2025-07-04 22:29:18 +0530

    Update to_do_list.py

--- to_do_list.py
+++ to_do_list.py
@@ -14,7 +14,7 @@ label1.grid(row=1, column=0, padx=10, pady=5, sticky=W)
 task_entry=Entry(root, font=('Arial', 12), width=30)
 task_entry.grid(row=1, column=1, padx=10, pady=5)
 
-fram= Frame(root)
+fram=Frame(root)
 fram.grid(row=2, column=0, columnspan=2, padx=10, pady=5)
 
 task_listbox=Listbox(
@@ -30,17 +30,28 @@ task_listbox.pack()
 def add_task():
     task=task_entry.get()
     if task!="":
-        task_listbox.insert(END, task)
+        task_listbox.insert(END, "❌ "+task)
         task_entry.delete(0, END)
 
 def delete_task():
-    selected= task_listbox.curselection()
+    selected=task_listbox.curselection()
     if selected:
         task_listbox.delete(selected[0])
+
+def mark_done():
+    selected=task_listbox.curselection()
+    if selected:
+        task=task_listbox.get(selected[0])
+        if task.startswith("❌"):
+            done_task="✅"+task[1:]
+            task_listbox.delete(selected[0])
+            task_listbox.insert(selected[0], done_task)
 
 btn1=Button(root, text="Add Task", command=add_task, bg='white', width=12)
 btn1.grid(row=3, column=0, pady=10)
 btn2=Button(root, text="Delete Task", command=delete_task, bg='white', width=12)
 btn2.grid(row=3, column=1, pady=10)
+btn3=Button(root, text="Mark as Done", command=mark_done, bg='white', width=25)
+btn3.grid(row=4, column=0, columnspan=2, pady=5)
 
 root.mainloop()
